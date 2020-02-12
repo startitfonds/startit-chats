92918342cceaa323fd00bc01028a44bdefe78378
juris124 <31391698+juris124@users.noreply.github.com>
2020-02-12 21:17:31 +0200

    limits_0.1

--- chats.py
+++ chats.py
@@ -17,5 +17,6 @@ def lasi(adresats):
 
 
 def pieraksti_zinju(dati):
+    dati["chats"]["zinja"] = dati["chats"]["zinja"][0:140] # limitējam ziņas garumu 
     with open(LOGFAILS, "a", newline="", encoding="utf-8") as f:
         f.write(json.dumps(dati["chats"]) + "\n")
